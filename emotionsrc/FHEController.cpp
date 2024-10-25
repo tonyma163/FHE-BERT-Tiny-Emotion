2b2c8c73986ee951c43305aae02cab59ab7d68fd
Tony Ma <tonyma@Tonys-MacBook-Pro.local>
2024-10-25 21:10:41 +0800

    evaluate each label accuracy (the rest combination)

--- emotionsrc/FHEController.cpp
+++ emotionsrc/FHEController.cpp
@@ -1290,11 +1290,11 @@ Ctxt FHEController::eval_inverse(const Ctxt &c, double min, double max) {
 }
 
 Ctxt FHEController::eval_inverse_naive(const Ctxt &c, double min, double max) {
-    return context->EvalChebyshevFunction([](double x) -> double { return 1 / x; }, c, min, max, 181); // 181 // 119
+    return context->EvalChebyshevFunction([](double x) -> double { return 1 / x; }, c, min, max, 289); // 181
 }
 
 Ctxt FHEController::eval_inverse_naive_2(const Ctxt &c, double min, double max, double mult) {
-    return context->EvalChebyshevFunction([mult](double x) -> double { return mult / x; }, c, min, max, 326); // 326 // 228
+    return context->EvalChebyshevFunction([mult](double x) -> double { return mult / x; }, c, min, max, 228); // 326 // 228
 }
 
 Ctxt FHEController::eval_gelu_function(const Ctxt &c, double min, double max, double mult, int degree) {
